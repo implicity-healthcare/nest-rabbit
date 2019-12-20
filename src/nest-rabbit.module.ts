027085e8e72c0e4250a9538f8730a5d60ce0254d
Julien Sarazin <julien.sarazin@me.com>
2019-12-20 09:56:10 +0100

    updated configuration handler

--- src/nest-rabbit.module.ts
+++ src/nest-rabbit.module.ts
@@ -1,23 +1,25 @@
 import { DynamicModule, Global, Module } from '@nestjs/common';
 import { NestRabbitService } from './nest-rabbit.service';
 import { INRModuleConfiguration } from './interfaces';
-import { NestRabbitMQConfigurationProvider, NestRabbitMQConnectionProvider, NestRabbitMQServiceProvider } from './constants';
-import {  AmqpConnectionManager } from 'amqp-connection-manager';
+import {
+    NestRabbitMQConfigurationProvider,
+    NestRabbitMQConnectionProvider,
+    NestRabbitMQServiceProvider
+} from './constants';
+import { AmqpConnectionManager } from 'amqp-connection-manager';
 import * as amqp from 'amqp-connection-manager';
+import { ConfigService } from '@nestjs/config';
 
 @Global()
 @Module({})
 export class NestRabbitModule {
 
-    static init(configuration: INRModuleConfiguration): DynamicModule {
+    static init(configuration?: INRModuleConfiguration): DynamicModule {
+
         const rabbitConfigurationProvider = {
             provide: NestRabbitMQConfigurationProvider,
-            useFactory: (): INRModuleConfiguration => {
-                /**
-                 * TODO: lean the configuration with default properties.
-                 */
-                return configuration;
-            },
+            useFactory: (configService: ConfigService): INRModuleConfiguration => configService.get<INRModuleConfiguration>('NRabbit', configuration),
+            inject: [ConfigService]
         };
 
         /**
@@ -25,9 +27,8 @@ export class NestRabbitModule {
          */
         const rabbitConnectionProvider = {
             provide: NestRabbitMQConnectionProvider,
-            useFactory: async (): Promise<any> => {
-                return await amqp.connect(configuration.urls, configuration.options);
-            },
+            useFactory: async (configuration: INRModuleConfiguration): Promise<any> => amqp.connect(configuration.urls, configuration.options),
+            inject: [NestRabbitMQConfigurationProvider]
         };
 
         const rabbitServiceProvider = {
